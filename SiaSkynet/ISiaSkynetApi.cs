5f97fac2f80d3c5c07969a24bf5fe6ff4f073be0
michielpost <michiel@q42.nl>
2020-05-25 10:02:00 +0200

    Project structure

---
+++ SiaSkynet/ISiaSkynetApi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiaSkyNet
+{
+    public interface ISiaSkynetApi
+    {
+    }
+}
---
+++ SiaSkynet/SiaSkynetClient.cs
@@ -0,0 +1,25 @@
+﻿using RestEase;
+using System;
+
+namespace SiaSkyNet
+{
+    public class SiaSkynetClient
+    {
+        private const string apiBaseUrl = "https://siasky.net/";
+
+        public static ISiaSkynetApi GetClient(string baseUrl = apiBaseUrl)
+        {
+            var nbApi = new RestClient(baseUrl)
+            {
+
+                //JsonSerializerSettings = new JsonSerializerSettings()
+                //{
+                //    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                //    NullValueHandling = NullValueHandling.Ignore,
+                //    Converters = new List<JsonConverter> { new StringEnumConverter() }
+                //}
+            }.For<ISiaSkynetApi>();
+            return nbApi;
+        }
+    }
+}
